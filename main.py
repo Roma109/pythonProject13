babb25c1d2a6610824da00e62ecef0f832c595eb
User <romaneefimoff@yandex.ru>
2022-11-17 18:42:53 +0400

    задание 3

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import sys
 from random import randint
+
 from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 SCREEN_SIZE = [500, 500]
+
+
+class Interface:
+
+    def loadUI(self, qwidget):
+        qwidget.setGeometry(500, 500, SCREEN_SIZE[0], SCREEN_SIZE[1])
+        qwidget.button = QPushButton('Кнопка', qwidget)
+        qwidget.button.move(20, 20)
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        Interface().loadUI(self)
         self.button.clicked.connect(self.paint)
         self.do_paint = False
 
@@ -23,8 +31,8 @@ class MyWidget(QWidget):
 
     def draw_star(self, qp):
         d = randint(3, 200)
-        heigh, width = randint(0, 300 - d), randint(0, 300 - d)
-        qp.setPen(QPen(QColor(255, 255, 0), 4))
+        heigh, width = randint(0, 500 - d), randint(0, 500 - d)
+        qp.setPen(QPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)), 4))
         qp.drawEllipse(heigh, width, d, d)
 
     def paint(self):
